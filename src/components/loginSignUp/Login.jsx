cff6dc0594700c6073d4e3da92e44cc7c5fe02da
Kelvin Ohaya <kelvinohaya@gmail.com>
2025-09-23 16:25:01 -0400

    preventing page from reload

--- src/components/loginSignUp/Login.jsx
+++ src/components/loginSignUp/Login.jsx
@@ -30,6 +30,7 @@ function Login({ setMode }) {
       console.log("Login response:", loginStatus);
       if (loginStatus === 200) {
         console.log("Login successful, navigating...");
+        return;
         // const last = "/chatroom";
         // window.location.href = last;
       } else {
